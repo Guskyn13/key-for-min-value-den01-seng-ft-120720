107fc0d39f1904dc778d8da7ac9ab8db0939ce86
Nathan K Gusky <n.gusky88@gmail.com>
2020-12-02 22:10:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 hash.each do |name, num|
   num < num += 1
-  if num is < num += 1
+  if num < num += 1
     puts "#{num}"
 end
 end
